cdb36b5de221165ab81952c33d10fa1ab9080241
Muhammed <muhammed@elemanuzmani.com>
2021-05-15 13:21:15 +0300

    Add project files.

---
+++ WinrarCompressExample/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinrarCompressExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //output rar file save location
+            string targetArchiveName = Path.Combine("d:/test", "CompressedFileName.rar");
+
+            //file list to compressing
+            List<string> filePathList = new List<string>();
+            filePathList.Add(Path.Combine("d:/test", "file.pdf"));
+
+            ProcessStartInfo startInfo = new ProcessStartInfo("WinRAR.exe");
+            startInfo.WindowStyle = ProcessWindowStyle.Maximized;
+
+            string arguments = string.Format("a -ep \"{0}\" ",
+                                  targetArchiveName);
+            
+            //
+            foreach (var yedekVeri in filePathList)
+            {
+                arguments += string.Format(" \"{0}\" ", yedekVeri);
+            }
+            startInfo.Arguments = arguments;
+
+            try
+            {
+                using (Process exeProcess = Process.Start(startInfo))
+                {
+                    exeProcess.WaitForExit();
+                }
+            }
+            catch (Exception)
+            { }
+        }
+    }
+}
